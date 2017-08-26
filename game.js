60561c1f71cb086f35007c81eda68bd3e8b0994a
中西美菜子 <minako@nakanishiminakonoMacBook-Pro.local>
2017-08-26 20:09:01 +0900

    ヘルパー関数の作成

--- game.js
+++ game.js
@@ -11,5 +11,13 @@ while (funds > 1 && funds < 100) {
 
 // m以上n以下のランダムな整数を返す.
 function rand(m, n) {
+    /**Math.floor()は引数として与えた数以下の最大の整数を返します。(小数点切り捨て)
+     * Math.rando()はoから1の範囲の中で擬似乱数を返す
+     */
     return m + Math.floor((n - m + 1) * Math.random());
+}
+
+//サイコロの目のどれか一つを表す文字列をランダムに返す.
+function randFace() {
+    return ["crown", "anchor", "heart", "spade", "club", "diamond"][rand(0, 5)];
 }
