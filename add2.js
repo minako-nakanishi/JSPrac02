93bc5319be6984fe304d4180d556532647f0e586
中西美菜子 <minako@nakanishiminakonoMacBook-Pro.local>
2017-08-27 23:36:42 +0900

    論理演算子について

--- add2.js
+++ add2.js
@@ -6,3 +6,21 @@ x += y = 6 * 5 / 2;
 // x += 15;
 //x = 3 +15;
 console.log(x);
+
+//等価演算子・厳密等価演算子
+const n = 5;
+const s = "5";
+console.log(n === s); //型が異なるなめfalse
+console.log(n == s); //true
+
+/**NaNは(NaNも含めて)どの値とも等しくならない */
+console.log(NaN == NaN); //false
+console.log(NaN === NaN); //false
+console.log(isNaN(NaN)); //isNaN()は引数がNaNかを判断する。NaNならtrue.
+
+//文字連結
+console.log(3 + 5 + "8"); //88
+console.log("3" + 5 + 8); //"35" + 8 -> 358
+
+if (undefined) console.log("t");
+else console.log("f");
