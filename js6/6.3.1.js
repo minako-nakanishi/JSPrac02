76570e491990bd9b988351a0d6fb44092a527546
Minako Nakanishi <n135216e@gmail.com>
2017-09-20 17:52:55 +0900

    展開演算子を用いた引数の分割代入

--- js6/6.3.1.js
+++ js6/6.3.1.js
@@ -23,3 +23,13 @@ function fruits([subject, verb, object]) {
 
 const m = ["You", "eat", "Apples"];
 console.log(fruits(m));
+
+/** 展開演算子を用いた引数の分割代入 */
+function addPrefix(prefix, ...words) {
+    const prefixWords = []; //初期化
+    for (let i = 0; i < words.length; i++) { //wordsの数分回す。
+        prefixWords[i] = prefix + words[i]; //接頭語とforで回したwordsを繋げる
+    }
+    return prefixWords;
+}
+console.log(addPrefix("Ap", "ple", "licot")); //[Apple, Aplicot]
