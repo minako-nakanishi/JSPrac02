bc7342540525e42e60e3acea313e9db3c702e406
Minako Nakanishi <n135216e@gmail.com>
2017-09-21 23:13:45 +0900

    オブジェクトのメソッド

--- js6/6.3.3.js
+++ js6/6.3.3.js
@@ -5,3 +5,18 @@ function test(a, b = "default", c = 3) {
 console.log(test(8, 7, 5)); //8 - 7 - 5
 console.log(test(5, 4)); //5 - 4 - 3
 console.log(test(7)); //7 - default - 3
+
+//オブジェクトのメソッド
+const mess = {
+    name: 'John',
+    bark: function() { return 'Wolf'; },
+}
+console.log(mess.name); //John
+console.log(mess.bark()); //Wolf
+
+//メソッドの省略
+const efc = {
+    name: 'Tama',
+    bark() { return 'Meaw'; },
+}
+console.log(efc.bark()); //Meaw
