36552653cbf671042dd57002c7093e2b1c754e1c
Minako Nakanishi <n135216e@gmail.com>
2017-10-15 22:55:23 +0900

    thisをネストの中で使う例

--- js6/this.js
+++ js6/this.js
@@ -16,3 +16,20 @@ console.log("speak() " + speak()); //My name is undefined
 console.log("speak " + speak); //speak() { return `My name is ${this.name}`; }
 console.log("o.speak " + o.speak); //speak() { return `My name is ${this.name}`; }
 console.log("o.speak() " + o.speak()) //My name is John
+
+const m = {
+    name: 'Cathy',
+    greetBackwards: function() {
+        const self = this;
+
+        function getReverseName() {
+            let nameBackwards = '';
+            for (let i = self.name.length - 1; i >= 0; i--) {
+                nameBackwards += self.name[i];
+            }
+            return nameBackwards;
+        } //getReverseNameの定義終わり
+        return `${getReverseName()} si eman ym ,olleH`;
+    },
+};
+console.log(m.greetBackwards());
