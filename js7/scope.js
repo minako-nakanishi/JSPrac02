78c711f5aa4161b221b6d93cf3bb506769a93d1f
Minako Nakanishi <n135216e@gmail.com>
2017-10-29 23:55:32 +0900

    var 宣言の巻き上げ

---
+++ js7/scope.js
@@ -0,0 +1,17 @@
+let var1;
+let var2 = undefined;
+console.log(var1); //undefined
+console.log(var2); //undefined
+// console.log(undef); //ReferenceError: undef is not defined
+
+// x; //ReferenceError: x is not defined
+let x = 3;
+
+y;
+var y = 3;
+// console.log(y); //3
+
+var z;
+console.log(z); //undefined
+z = 4;
+console.log(z); //4
